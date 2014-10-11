71e4b231c4f76d74e120c56decdd2ed50b4159f2
Anurag Kyal <anuragkyal2008@gmail.com>
2014-10-11 14:58:53 -0400

    Adding the classifier

--- Project1/main/main.py
+++ Project1/main/main.py
@@ -3,7 +3,10 @@ __author__ = 'Luke'
 import math
 import numpy as np
 from sklearn import cross_validation
+from sklearn import tree
+from sklearn.datasets import load_iris
 from sklearn import datasets
+from sklearn.externals.six import StringIO
 
 
 def main():
@@ -39,6 +42,24 @@ def main():
         # Predict the value based on the classifier
         prediction = classifier.predict(testSet)
 
+def getTrainingDataLeaveOneOut(rawMatrix, ignoredUser):
+    #getting all the samples for all users except the ignored one
+    relevantMatrix = rawMatrix[rawMatrix[:,0] != ignoredUser, :]
+
+    #remove the label column
+    featureMatrix = relevantMatrix[:, 0:-1]
+    #label column
+    valueArray = relevantMatrix[:, -1]
+
+    return (featureMatrix, valueArray)
+
+def getClassifier(samples, target):
+    clf = tree.DecisionTreeClassifier()
+    return clf.fit(samples, target)
+
+def getClassifierLeaveOneOut(rawMatrix, ignoredUser):
+    trainingData = getTrainingDataLeaveOneOut(rawMatrix, ignoredUser)
+    return getClassifier(trainingData[0], trainingData[1])
 
 if __name__ == "__main__":
     main()
